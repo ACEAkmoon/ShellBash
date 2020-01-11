1bf23f4b95d589ba9cc0fdf9e3fd10860dfb56a2
ACEAkmoon <oleksandr.krushynskyi@whaleapp.com>
2020-01-11 16:16:17 +0200

    addFind

--- clearXcodeMacOS.sh
+++ clearXcodeMacOS.sh
@@ -1,4 +1,5 @@
+cd // && \
+	find . \( -name "#*#" -o -name "*/" -o -name "*~" \) -print -delete && \
 cd //Users/creativetrendadmin/Library/Developer/Xcode && \
 	find . \( -name "Unity-iPhone-*" \) -print -delete
-
 
