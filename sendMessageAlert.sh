bcda3d26379a4b614ee2eaeed171ccafab09327f
aceakmoon <acealex@ukr.net>
2020-02-09 15:09:11 +0200

    while

--- sendMessageAlert.sh
+++ sendMessageAlert.sh
@@ -9,7 +9,7 @@ while [ $TIME -gt 0 ]
                 sleep 1
         done
 
-curl -s -X POST "https://api.telegram.org/bot1094799241:AAEJNvcAKuS4VvRvwN1ysjQHAT0srbQaCSQ/sendMessage" -F chat_id="843569800" -F text="BITCH! Some hell restarted your PC"
+curl -s -X POST "https://api.telegram.org/bot<TOKEN:TOKEN>/sendMessage" -F chat_id="CHAT_ID" -F text="BITCH! Some hell restarted your PC"
 
 echo ''
 echo Well Done!
